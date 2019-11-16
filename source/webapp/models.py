f478ea5261882d638225973b1fc77d365457e2b7
Ruslan <h.ruslan97@gmail.com>
2019-11-16 13:06:56 +0600

    Added models Product and Review and migrate

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+RATING_CHOICE = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5')
+)
+CATEGORY_CHOICE = (
+    ('Phones', 'Телефоны'),
+    ('Notebook', 'Ноутбуки'),
+    ('Bike', 'Велосипеды')
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=25, verbose_name='Название')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICE, verbose_name='Категории')
+    description = models.TextField(max_length=500, null=True, blank=True, verbose_name='Описание')
+    picture = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Картинка')
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, related_name='users', on_delete=models.CASCADE, verbose_name='Автор')
+    product = models.ForeignKey('webapp.Product', related_name='products', on_delete=models.CASCADE, verbose_name='Товар')
+    text = models.TextField(max_length=500, verbose_name='Текст отзыва')
+    rating = models.IntegerField(choices=RATING_CHOICE, verbose_name='Оценка')
+
+    def __str__(self):
+        return '{} -- {}'.format(self.author, self.product)
+
